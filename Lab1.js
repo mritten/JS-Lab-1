b0b02194f5dff9abd9271cc75196587554b844d8
mritten <mritten@gmail.com>
2018-08-01 18:29:01 -0400

    Javascript Lab 2

---
+++ Lab1.js
@@ -0,0 +1,43 @@
+let Name1 = prompt('1. Enter the first and last name'):
+let Name2 = prompt('2. Enter the first and last name'):
+let Name3 = prompt('3. Enter the first and last name'):
+
+if (Name1.length == Name2.length || && Name3.length) {
+  console.log(Name1,Name2,Name3):
+} 
+else (Name1.length == Name2.length) {
+  console.log(Name1, Name2)
+} 
+if (Name1.length == Name3.length) {
+  console.log (Name1,Name3)
+}
+else (Name2.length == Name1.length) {
+  console.log (Name2, Name1)
+  }
+if (Name2.length == Name3.length) {
+  console.log (Name2, Name3)
+  }
+else (Name3.length == Name1.length) {
+  console.log (Name3, Name1)
+  }
+if (Name3.length = Name2.length) {
+  console.log (Name3, Name2)
+  }
+else (Name1.length > Name2.length) {
+  console.log(Name1)
+} 
+if (Name1.length > Name3.length) {
+  console.log (Name1)
+}
+else (Name2.length > Name1.length) {
+  console.log (Name2)
+  }
+if (Name2.length > Name3.length) {
+  console.log (Name2)
+}
+else (Name3.length > Name1.length) {
+  console.log (Name3)
+  }
+if (Name3.length > Name2.length) {
+  console.log (Name3)
+}
